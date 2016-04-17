2a89f91342817a9cc64703e6a4794c6d49876b34
Nina <nina.pikulska@gmail.com>
2016-04-17 15:22:10 +0200

    Coordinate system definition

---
+++ src/src/CoordinateSystem.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class CoordinateSystem {
+    List<Vector> picture;
+
+    public CoordinateSystem() {
+        this.picture = new LinkedList<Vector>();
+    }
+
+    public List<Vector> getPicture() {
+        return picture;
+    }
+
+    public void setPicture(List<Vector> picture) {
+        this.picture = picture;
+    }
+}
